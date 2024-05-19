bfcea8da0bc2f609e87989e89bf2a5d430c9d197
Vally1998 <164740279+Vally1998@users.noreply.github.com>
2024-05-19 13:56:34 +0300

    func1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿string[] Array = new string[4] {"1234", "1567", "-2", "computer science"};
 string[] newArray = new string[Array.Length];
+void myArray(string[] Array, string[] newArray)
+{
+    int count = 0;
+    for (int i = 0; i < Array.Length; i++)
+    {
+    if(Array[i].Length <= 3)
+        {
+        newArray[i] = Array[i];
+        count++;
+        }
+    }
+} 
